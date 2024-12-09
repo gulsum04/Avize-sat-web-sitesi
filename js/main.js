86b8148bd58f7f17f773d4ffed9cc1ffc80ba0e1
gulsum04 <149891161+gulsum04@users.noreply.github.com>
2024-12-09 12:26:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,336 @@
+(function ($) {
+  "use strict";
+
+  /*[ Load page ]
+    ===========================================================*/
+  $(".animsition").animsition({
+    inClass: "fade-in",
+    outClass: "fade-out",
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: ".animsition-link",
+    loading: true,
+    loadingParentElement: "html",
+    loadingClass: "animsition-loading-1",
+    loadingInner: '<div class="loader05"></div>',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: ["animation-duration", "-webkit-animation-duration"],
+    overlay: false,
+    overlayClass: "animsition-overlay-slide",
+    overlayParentElement: "html",
+    transition: function (url) {
+      window.location.href = url;
+    },
+  });
+
+  /*[ Back to top ]
+    ===========================================================*/
+  var windowH = $(window).height() / 2;
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > windowH) {
+      $("#myBtn").css("display", "flex");
+    } else {
+      $("#myBtn").css("display", "none");
+    }
+  });
+
+  $("#myBtn").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 300);
+  });
+
+  /*==================================================================
+    [ Fixed Header ]*/
+  var headerDesktop = $(".container-menu-desktop");
+  var wrapMenu = $(".wrap-menu-desktop");
+
+  if ($(".top-bar").length > 0) {
+    var posWrapHeader = $(".top-bar").height();
+  } else {
+    var posWrapHeader = 0;
+  }
+
+  if ($(window).scrollTop() > posWrapHeader) {
+    $(headerDesktop).addClass("fix-menu-desktop");
+    $(wrapMenu).css("top", 0);
+  } else {
+    $(headerDesktop).removeClass("fix-menu-desktop");
+    $(wrapMenu).css("top", posWrapHeader - $(this).scrollTop());
+  }
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > posWrapHeader) {
+      $(headerDesktop).addClass("fix-menu-desktop");
+      $(wrapMenu).css("top", 0);
+    } else {
+      $(headerDesktop).removeClass("fix-menu-desktop");
+      $(wrapMenu).css("top", posWrapHeader - $(this).scrollTop());
+    }
+  });
+
+  /*==================================================================
+    [ Menu mobile ]*/
+  $(".btn-show-menu-mobile").on("click", function () {
+    $(this).toggleClass("is-active");
+    $(".menu-mobile").slideToggle();
+  });
+
+  var arrowMainMenu = $(".arrow-main-menu-m");
+
+  for (var i = 0; i < arrowMainMenu.length; i++) {
+    $(arrowMainMenu[i]).on("click", function () {
+      $(this).parent().find(".sub-menu-m").slideToggle();
+      $(this).toggleClass("turn-arrow-main-menu-m");
+    });
+  }
+
+  $(window).resize(function () {
+    if ($(window).width() >= 992) {
+      if ($(".menu-mobile").css("display") == "block") {
+        $(".menu-mobile").css("display", "none");
+        $(".btn-show-menu-mobile").toggleClass("is-active");
+      }
+
+      $(".sub-menu-m").each(function () {
+        if ($(this).css("display") == "block") {
+          console.log("hello");
+          $(this).css("display", "none");
+          $(arrowMainMenu).removeClass("turn-arrow-main-menu-m");
+        }
+      });
+    }
+  });
+
+  /*==================================================================
+    [ Show / hide modal search ]*/
+  $(".js-show-modal-search").on("click", function () {
+    $(".modal-search-header").addClass("show-modal-search");
+    $(this).css("opacity", "0");
+  });
+
+  $(".js-hide-modal-search").on("click", function () {
+    $(".modal-search-header").removeClass("show-modal-search");
+    $(".js-show-modal-search").css("opacity", "1");
+  });
+
+  $(".container-search-header").on("click", function (e) {
+    e.stopPropagation();
+  });
+
+  /*==================================================================
+    [ Isotope ]*/
+  var $topeContainer = $(".isotope-grid");
+  var $filter = $(".filter-tope-group");
+
+  // filter items on button click
+  $filter.each(function () {
+    $filter.on("click", "button", function () {
+      var filterValue = $(this).attr("data-filter");
+      $topeContainer.isotope({ filter: filterValue });
+    });
+  });
+
+  // init Isotope
+  $(window).on("load", function () {
+    var $grid = $topeContainer.each(function () {
+      $(this).isotope({
+        itemSelector: ".isotope-item",
+        layoutMode: "fitRows",
+        percentPosition: true,
+        animationEngine: "best-available",
+        masonry: {
+          columnWidth: ".isotope-item",
+        },
+      });
+    });
+  });
+
+  var isotopeButton = $(".filter-tope-group button");
+
+  $(isotopeButton).each(function () {
+    $(this).on("click", function () {
+      for (var i = 0; i < isotopeButton.length; i++) {
+        $(isotopeButton[i]).removeClass("how-active1");
+      }
+
+      $(this).addClass("how-active1");
+    });
+  });
+
+  /*==================================================================
+    [ Filter / Search product ]*/
+  $(".js-show-filter").on("click", function () {
+    $(this).toggleClass("show-filter");
+    $(".panel-filter").slideToggle(400);
+
+    if ($(".js-show-search").hasClass("show-search")) {
+      $(".js-show-search").removeClass("show-search");
+      $(".panel-search").slideUp(400);
+    }
+  });
+
+  $(".js-show-search").on("click", function () {
+    $(this).toggleClass("show-search");
+    $(".panel-search").slideToggle(400);
+
+    if ($(".js-show-filter").hasClass("show-filter")) {
+      $(".js-show-filter").removeClass("show-filter");
+      $(".panel-filter").slideUp(400);
+    }
+  });
+
+  /*==================================================================
+    [ Cart ]*/
+  $(".js-show-cart").on("click", function () {
+    $(".js-panel-cart").addClass("show-header-cart");
+  });
+
+  $(".js-hide-cart").on("click", function () {
+    $(".js-panel-cart").removeClass("show-header-cart");
+  });
+
+  /*==================================================================
+    [ Cart ]*/
+  $(".js-show-sidebar").on("click", function () {
+    $(".js-sidebar").addClass("show-sidebar");
+  });
+
+  $(".js-hide-sidebar").on("click", function () {
+    $(".js-sidebar").removeClass("show-sidebar");
+  });
+
+  /*==================================================================
+    [ +/- num product ]*/
+  $(".btn-num-product-down").on("click", function () {
+    var numProduct = Number($(this).next().val());
+    if (numProduct > 0)
+      $(this)
+        .next()
+        .val(numProduct - 1);
+  });
+
+  $(".btn-num-product-up").on("click", function () {
+    var numProduct = Number($(this).prev().val());
+    $(this)
+      .prev()
+      .val(numProduct + 1);
+  });
+
+  /*==================================================================
+    [ Rating ]*/
+  $(".wrap-rating").each(function () {
+    var item = $(this).find(".item-rating");
+    var rated = -1;
+    var input = $(this).find("input");
+    $(input).val(0);
+
+    $(item).on("mouseenter", function () {
+      var index = item.index(this);
+      var i = 0;
+      for (i = 0; i <= index; i++) {
+        $(item[i]).removeClass("zmdi-star-outline");
+        $(item[i]).addClass("zmdi-star");
+      }
+
+      for (var j = i; j < item.length; j++) {
+        $(item[j]).addClass("zmdi-star-outline");
+        $(item[j]).removeClass("zmdi-star");
+      }
+    });
+
+    $(item).on("click", function () {
+      var index = item.index(this);
+      rated = index;
+      $(input).val(index + 1);
+    });
+
+    $(this).on("mouseleave", function () {
+      var i = 0;
+      for (i = 0; i <= rated; i++) {
+        $(item[i]).removeClass("zmdi-star-outline");
+        $(item[i]).addClass("zmdi-star");
+      }
+
+      for (var j = i; j < item.length; j++) {
+        $(item[j]).addClass("zmdi-star-outline");
+        $(item[j]).removeClass("zmdi-star");
+      }
+    });
+  });
+
+  /*==================================================================
+    [ Show modal1 ]*/
+  $(".js-show-modal1").on("click", function (e) {
+    e.preventDefault();
+    $(".js-modal1").addClass("show-modal1");
+  });
+
+  $(".js-hide-modal1").on("click", function () {
+    $(".js-modal1").removeClass("show-modal1");
+  });
+
+  /*==================================================================
+    [Pop-up]*/
+  $(document).ready(function () {
+    /* Popup Açma Fonksiyonu */
+    function openPopup(title, description, pdfUrl = null, imageUrl = null) {
+      $("#popup-title").text(title);
+      $("#popup-description").text(description);
+      $("#popup").css("display", "flex");
+
+      // PDF URL'si varsa "PDF İndir" butonunu göster ve bağlantıyı ekle
+      if (pdfUrl) {
+        $("#pdf-download-btn").css("display", "inline-block"); // Butonu görünür yap
+        $("#pdf-download-btn").data("pdf-url", pdfUrl); // Butona PDF URL'sini ata
+      } else {
+        $("#pdf-download-btn").css("display", "none"); // PDF yoksa butonu gizle
+      }
+
+      // Resim URL'si varsa resmi göster ve kaynağını ayarla
+      if (imageUrl) {
+        $("#popup-image").attr("src", imageUrl).css("display", "block");
+      } else {
+        $("#popup-image").css("display", "none"); // Resim yoksa gizle
+      }
+    }
+
+    /* Popup Kapatma Fonksiyonu */
+    function closePopup() {
+      $("#popup").css("display", "none");
+    }
+
+    // Duyuru öğelerine tıklayınca pop-up aç
+    $(".announcement-item").on("click", function () {
+      const title = $(this).data("title");
+      const description = $(this).data("description");
+      const pdfUrl = $(this).data("pdf"); // PDF URL'sini data-pdf'ten al
+      const imageUrl = $(this).data("image"); // Resim URL'sini data-image'ten al
+      openPopup(title, description, pdfUrl, imageUrl);
+    });
+
+    // "PDF İndir" butonuna tıklanınca PDF dosyasını indir
+    $("#pdf-download-btn").on("click", function () {
+      const pdfUrl = $(this).data("pdf-url");
+      if (pdfUrl) {
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = pdfUrl.split("/").pop(); // Dosya adını indirilecek PDF'den alır
+        link.click();
+      }
+    });
+
+    // Kapatma butonuna tıklayınca pop-up'ı kapat
+    $(".popup-close").on("click", function () {
+      closePopup();
+    });
+
+    // Popup arka plana tıklayınca kapat
+    $(".popup").on("click", function (e) {
+      if (e.target === this) {
+        closePopup();
+      }
+    });
+  });
+})(jQuery);
